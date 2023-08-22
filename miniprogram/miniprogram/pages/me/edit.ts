2b09a0dfe1782b49fff3d12403859206ff7c0d3b
yansongda <me@yansongda.cn>
2023-08-22 17:56:45 +0800

    update

--- miniprogram/miniprogram/pages/me/edit.ts
+++ miniprogram/miniprogram/pages/me/edit.ts
@@ -2,21 +2,46 @@ const app = getApp<GlobalData>()
 
 Page({
   data: {
-    user: app.globalData.user,
+    avatar: app.globalData.user.avatar,
+    nickname: app.globalData.user.nickname,
+    slogan: app.globalData.user.slogan,
+  },
+  onShow() {
+    this.setData({
+      avatar: app.globalData.user.avatar,
+      nickname: app.globalData.user.nickname,
+      slogan: app.globalData.user.slogan,
+    })
   },
   onChooseAvatar(e: any) {
     const { avatarUrl } = e.detail 
     
-    // todo: 上传文件到服务端并更新
+    // todo: 上传文件到服务端并更新当前用户的头像
     app.globalData.user.avatar = avatarUrl
 
     this.setData({
-      user: app.globalData.user
+      avatar: avatarUrl
     })
   },
   submit(e: any) {
-    // todo: 提交表单，更新用户数据
-    console.log(e.detail.value)
+    const updated = e.detail.value
+
+    // todo: 提交表单
+    console.log(updated)
+
+    wx.showToast({
+      title: '修改成功',
+      icon: 'success',
+      duration: 1500,
+      success: () => {
+        app.globalData.user.nickname = updated.nickname
+        app.globalData.user.slogan = updated.slogan
+
+        setTimeout(() => {
+          wx.navigateBack()          
+        }, 1500);
+      }
+    })
   },
 })
 
--- miniprogram/miniprogram/pages/me/index.ts
+++ miniprogram/miniprogram/pages/me/index.ts
@@ -2,12 +2,23 @@ const app = getApp<GlobalData>()
 
 Page({
   data: {
-    user: app.globalData.user,
+    avatar: app.globalData.user.avatar,
+    nickname: app.globalData.user.nickname,
+    slogan: app.globalData.user.slogan,
   },
   onLoad() {
   },
   onShow() {
+    this.setData({
+      avatar: app.globalData.user.avatar,
+      nickname: app.globalData.user.nickname,
+      slogan: app.globalData.user.slogan,
+    })
   },
+  onHide() {
+  },
+  onReady() {
+  }
 })
 
 export default {}
