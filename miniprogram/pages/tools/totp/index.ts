9fd0b305eecadc8e270c94de03924c57c8adf2e9
yansongda <me@yansongda.cn>
2023-08-24 18:40:55 +0800

    update

--- miniprogram/pages/tools/totp/index.ts
+++ miniprogram/pages/tools/totp/index.ts
@@ -5,7 +5,13 @@ Page({
         "id": 1,
         "username": "me@yansongda.cn",
         "issuer": "Vultr",
-        "code": "123 456"
+        "code": "123456"
+      },
+      {
+        "id": 2,
+        "username": "me@yansongda.cn",
+        "issuer": "Vultr",
+        "code": "123456"
       }
     ]
   },
