c7c9d769dcd99f70ccdbaafc95a3ced964878811
yansongda <me@yansongda.cn>
2024-03-08 23:22:22 +0800

    update

---
+++ miniprogram/custom-tab-bar/index.js
@@ -0,0 +1,24 @@
+Component({
+  data: {
+    value: 'pages/home/index',
+    list: [
+      { value: 'pages/home/index', label: '首页', icon: 'home' },
+      { value: 'pages/user/index', label: '我的', icon: 'user' }
+    ]
+  },
+  lifetimes: {
+    ready() {
+      const pages = getCurrentPages()
+      const curPage = pages[pages.length - 1]
+
+      this.setData({ value: curPage.route })
+    }
+  },
+  methods: {
+    onChange(e) {
+      const path = e.detail.value
+
+      wx.switchTab({ url: '/' + path })
+    }
+  }
+})
